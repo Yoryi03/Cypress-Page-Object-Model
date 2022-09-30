14a81b5d09b257496d68ade4189e580947cec6e9
Yorgi Rafael <yoryi.valdez03@gmail.com>
2022-09-30 09:12:06 -0400

    firs pr

---
+++ cypress/pages/contactFormPage.js
@@ -0,0 +1,33 @@
+class contactUsPage {
+  elements = {
+    titleSpan: () => cy.get(".title"),
+    subjectHeading: () => cy.get("#id_contact"),
+    emailAddress: () => cy.get("#email"),
+    orderReference: () => cy.get("#id_order"),
+    attachFile: () => cy.get("#fileUpload"),
+    comment: () => cy.get("#message"),
+    sendBtn: () => cy.get("#submitMessage"),
+    sucessAlert: () => cy.get(".alert-success"),
+    dangerAlert: () => cy.get(".alert-danger"),
+    uploadFile: () => cy.get("#fileUpload"),
+    fileName: () => cy.get(".filename"),
+  };
+
+  clickSend() {
+    this.elements.sendBtn().click();
+  }
+  selectSubject(subject) {
+    this.elements.subjectHeading().select(subject);
+  }
+  typeEmail(email) {
+    this.elements.emailAddress().type(email);
+  }
+  typeOrderReference(order) {
+    this.elements.orderReference().type(order);
+  }
+  typeMessage(message) {
+    this.elements.comment().type(message);
+  }
+}
+
+module.exports = new contactUsPage();
